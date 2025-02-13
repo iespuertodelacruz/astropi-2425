c650c91616425c10199758e825d15c4a814d0916
Carliaal <carlagarciapachecotf@gmail.com>
2025-02-13 10:37:06 +0000

    codigo carla

--- src/main.py
+++ src/main.py
@@ -33,10 +33,59 @@ else:
 # ==============================================================================
 # CODE
 # ==============================================================================
+HEADER = [
+    'timestamp',
+    'latitude',
+    'longitude',
+    'altitude',
+    'temperature',
+    'humidity',
+    'pressure',
+    'magnetic_field_x',
+    'magnetic_field_y',
+    'magnetic_field_z',
+    'acceleration_x',
+    'acceleration_y',
+    'acceleration_z',
+    'rotation_x',
+    'rotation_y',
+    'rotation_z',
+]
 sense = SenseHat()
-position = get_ISS_position()
-print(position)
 
-sense.set_imu_config(compass_enabled=True, gyro_enabled=False, accel_enabled=False)
-magnetic = sense.get_compass_raw()
-print(magnetic)
+
+with open('iss.csv', 'w') as folder:
+    folder.write(f'{','.join(HEADER)}\n')
+    
+    while True:
+        timestamp = strftime('%d-%b-%Y %H:%M:%S')
+        latitude, longitude, altitude = get_ISS_position().values()
+        temperature = sense.get_temperature()
+        humidity = sense.get_humidity()
+        pressure = sense.get_pressure()
+        
+        acceleration_x, acceleration_y, acceleration_z = sense.get_accelerometer_raw().values()
+        magnetic_x, magnetic_y, magnetic_z = sense.get_compass_raw().values()
+        rotation_x, rotation_y, rotation_z = sense.get_gyroscope_raw().values()
+        
+parameters = [ 
+    f'{timestamp}',
+    f'{latitude}',
+    f'{longitude}',
+    f'{altitude}',
+    f'{temperature}',
+    f'{humidity}',
+    f'{pressure}',
+    f'{magnetic_x}',
+    f'{magnetic_y}',
+    f'{magnetic_z}',
+    f'{acceleration_x}',
+    f'{acceleration_y}',
+    f'{acceleration_z}',
+    f'{rotation_x}',
+    f'{rotation_y}',
+    f'{rotation_z}', 
+]
+
+folder.write(f'{','.join(parameters)}\n')
+time.sleep(5)
