ea104a1b891e1ae9cf31bbbd56f28dc4698d634d
Aaron <aaralvrod@gmail.com>
2025-02-17 22:53:00 +0100

    Update content

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ DEBUG = True
 # ==============================================================================
 # IMPORTS
 # ==============================================================================
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 if DEBUG:
     from fake_sensors import SenseHat, get_ISS_position
@@ -35,22 +35,26 @@ else:
 # CODE
 # ==============================================================================
 sense = SenseHat()
-sense.set_imu_config(compass_enabled=True, gyro_enabled=False, accel_enabled=False)
 HEADER = ['timestamp', 'latitude', 'longitude', 'altitude', 'temperature', 'humidity', 'pressure', 'magnetic_x', 'magnetic_y', 'magnetic_z', 'acceleration_x', 'acceleration_y', 'acceleration_z', 'rotation_x', 'rotation_y', 'rotation_z']
 
 with open('iss.csv', 'w') as f:
     f.write(f'{",".join(HEADER)}\n')
+    start_time = datetime.now()
+    end_time = start_time + timedelta(hours=3)
     
-    while True:
+    while start_time < end_time:
         timestamp = datetime.now().isoformat()
-        latitude, longitude, altitude = get_ISS_position().values()
-        acceleration_x, acceleration_y, acceleration_z = sense.get_accelerometer_raw().values()
-        magnetic_x, magnetic_y, magnetic_z = sense.get_compass_raw().values()
-        rotation_x, rotation_y, rotation_z = sense.get_gyroscope_raw().values()
-        temperature = sense.get_temperature()
-        pressure = sense.get_pressure()
-        humidity = sense.get_humidity()
         position = get_ISS_position()
-        
-        f.write(f'{timestamp},{latitude},{longitude},{altitude},{temperature},{humidity},{pressure},{magnetic_x},{magnetic_y},{magnetic_z},{acceleration_x},{acceleration_y},{acceleration_z},{rotation_x},{rotation_y},{rotation_z}\n')
+        temperature = sense.get_temperature()
+        humidity = sense.get_humidity()
+        pressure = sense.get_pressure()
+        sense.set_imu_config(compass_enabled=True, gyro_enabled=False, accel_enabled=False)
+        magnetic = sense.get_compass_raw()
+        sense.set_imu_config(compass_enabled=False, gyro_enabled=True, accel_enabled=False)
+        rotation = sense.get_gyroscope_raw()
+        sense.set_imu_config(compass_enabled=False, gyro_enabled=False, accel_enabled=True)
+        acceleration = sense.get_accelerometer_raw()
+        stats = [timestamp, position["latitude"], position["longitude"], position["altitude"], temperature, humidity, pressure, magnetic["x"], magnetic["y"], magnetic["z"], acceleration["x"], acceleration["y"], acceleration["z"], rotation["x"], rotation["y"], rotation["z"]]
+        f.write(f'{",".join(stats)}\n')
+        f.flush()
         time.sleep(5)
